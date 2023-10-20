02ad87ffc77ac679f2437b1a33cba1f076167802
Habiba Hye <145866308+HabibiHye@users.noreply.github.com>
2023-10-20 12:17:50 -0400

    Update player.hpp

---
+++ player.hpp
@@ -0,0 +1,7 @@
+// Include the Board class header
+
+//Class Player
+
+//Set a Getter for player's name and score.
+
+//Keep incrementing the player's score and keep tracks of the move by the players.
