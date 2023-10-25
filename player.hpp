fa6b415105bf75178b1d288e96c08883be71ce6e
Habiba Hye <145866308+HabibiHye@users.noreply.github.com>
2023-10-25 12:13:24 -0400

    Update player.hpp

--- player.hpp
+++ player.hpp
@@ -25,3 +25,31 @@ public:
     void updatedScore(int points) {
         score += points;
     }
+
+    // Play a move in the game
+    void playMove(Board& board, int x, int y) {
+        // Implement the logic to play a move on the board
+        // You can call appropriate methods from the Board class
+    }
+};
+
+int main() {
+    // Example usage of the Player class
+    Player player1("habiba");
+    Player player2("Memo");
+
+    // Example game loop
+    Board board; // Assuming you have created a Board object
+    bool gameInProgress = true;
+
+    while (gameInProgress) {
+        // Get player input for the move (you can use your own logic)
+        int x, y;
+        std::cout << "Enter the coordinates (x, y) for your move: ";
+        std::cin >> x >> y;
+
+        // Perform the move
+        player1.playMove(board, x, y);
+
+    return 0;
+}
