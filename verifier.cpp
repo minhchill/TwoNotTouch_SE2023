d1ff3fabbe80d1cc791659c3a95fca799159d948
Minh Chill <chieuminh0507@gmail.com>
2023-10-30 09:27:01 -0400

    Some further verifier edits

---
+++ verifier.cpp
@@ -0,0 +1,40 @@
+// Testing out a design of a class to verify a solved puzzle 
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+// // Take a 10 x 10 grid with certain criteria as input and pass them into a list of vectors
+
+
+// // Pass that list of vectors into a list of sets to represent an irregular region
+std::vector<std::string> setRegions(const std::vector<std::vector<char>>& grid){
+    std::vector<std::string> regions;
+    return regions;
+}
+
+// // Check if 2 lower case letters (for 2 stars) are presented in a row or column
+// bool has2LowerCase(){
+    
+// }
+
+// // Check if 2 lower case letters are presented in a bounded region
+// bool 
+
+// // Correct solution 
+// bool isCorrectSolution() {
+//     return true;
+// }
+
+// Driver
+int main()
+{
+    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    for (const std::string& word : msg)
+    {
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
+}
