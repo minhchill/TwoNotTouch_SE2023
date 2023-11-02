60ab28505ab00bc2ea93b1d2dd452a912f6cc4cc
minhchill <chieuminh0507@gmail.com>
2023-11-01 23:28:41 -0400

    Some verifier stuff

--- verifier.cpp
+++ verifier.cpp
@@ -3,16 +3,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <iostream>
+#include <fstream>
+
 
 // // Take a 10 x 10 grid with certain criteria as input and pass them into a list of vectors
 
 
 // // Pass that list of vectors into a list of sets to represent an irregular region
-std::vector<std::string> setRegions(const std::vector<std::vector<char>>& grid){
-    std::vector<std::string> regions;
-    return regions;
-}
+// std::vector<std::string> setRegions(const std::vector<std::vector<char>>& grid){
+//     std::vector<std::string> regions;
+//     return regions;
+// }
 
 // // Check if 2 lower case letters (for 2 stars) are presented in a row or column
 // bool has2LowerCase(){
@@ -30,11 +32,26 @@ std::vector<std::string> setRegions(const std::vector<std::vector<char>>& grid){
 // Driver
 int main()
 {
-    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-    for (const std::string& word : msg)
-    {
-        std::cout << word << " ";
-    }
+    //open file, read contents to get puzzle
+    std::ifstream puzzle;
+    std::string line;
+    puzzle.open("testfilesolution.txt");
+    std::vector<std::string> g1;
+
+    if (puzzle.is_open()) {
+        while (std::getline(puzzle, line)) {
+            //std::cout << line << '\n';
+            g1.push_back(line);
+        }
+        puzzle.close();
+    }    
+
+    std::cout << "Output of vector: " << std::endl;
+    for (auto i = g1.begin(); i != g1.end(); i++)
+
+        std::cout << *i << std::endl;
+
     std::cout << std::endl;
+    return 0;
+
 }
