a99ae62a17af43f1225c6fb896d9b7d1ff0177a4
minhchill <chieuminh0507@gmail.com>
2023-11-03 11:19:57 -0400

    Some verifier edits

--- verifier.cpp
+++ verifier.cpp
@@ -6,22 +6,24 @@
 #include <iostream>
 #include <fstream>
 
+#include "manager.hpp"
+
 
 // // Take a 10 x 10 grid with certain criteria as input and pass them into a list of vectors
-
+// main driver
 
 // // Pass that list of vectors into a list of sets to represent an irregular region
-// std::vector<std::string> setRegions(const std::vector<std::vector<char>>& grid){
-//     std::vector<std::string> regions;
-//     return regions;
-// }
+std::vector<std::string> setRegions(const ){
+    std::vector<std::string> regions;
+    return regions;
+}
 
-// // Check if 2 lower case letters (for 2 stars) are presented in a row or column
+// // Check if 2 lower case letters (for 2 stars) are presented in a row or column (H)
 // bool has2LowerCase(){
     
 // }
 
-// // Check if 2 lower case letters are presented in a bounded region
+// // Check if 2 lower case letters are presented in a bounded region (H)
 // bool 
 
 // // Correct solution 
@@ -29,29 +31,44 @@
 //     return true;
 // }
 
-// Driver
+// Driver test
 int main()
 {
-    //open file, read contents to get puzzle
-    std::ifstream puzzle;
-    std::string line;
-    puzzle.open("testfilesolution.txt");
-    std::vector<std::string> g1;
+    std::ifstream puzzle ("testfilesolution.txt");
+    std::string mystring;
 
-    if (puzzle.is_open()) {
-        while (std::getline(puzzle, line)) {
-            //std::cout << line << '\n';
-            g1.push_back(line);
-        }
-        puzzle.close();
-    }    
-
-    std::cout << "Output of vector: " << std::endl;
-    for (auto i = g1.begin(); i != g1.end(); i++)
-
-        std::cout << *i << std::endl;
-
-    std::cout << std::endl;
-    return 0;
+    if (puzzle.is_open()){
+        puzzle >> mystring;
+        std::cout << mystring;
+    }
 
 }
+
+// Driver
+// int main()
+// {
+//     //open file, read contents to get puzzle
+//     std::ifstream puzzle;
+//     std::string line;
+//     puzzle.open("testfilesolution.txt");
+//     std::vector<std::string> g1;
+
+//     if (puzzle.is_open()) {
+//         while (std::getline(puzzle, line)) {
+//             //std::cout << line << '\n';
+//             g1.push_back(line);
+//         }
+//         puzzle.close();
+//     }    
+
+//     std::cout << isGridcharlegal(g1) << std::endl;
+
+//     std::cout << "Output of vector: " << std::endl;
+//     for (auto i = g1.begin(); i != g1.end(); i++)
+
+//         std::cout << *i << std::endl;
+
+//     std::cout << std::endl;
+//     return 0;
+
+// }
