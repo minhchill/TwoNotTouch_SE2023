69a21307760e8d693221774dd966ad301c5faf84
Habiba Hye <145866308+HabibiHye@users.noreply.github.com>
2023-11-03 12:44:58 -0400

    Update player.hpp

--- player.hpp
+++ player.hpp
@@ -49,19 +49,19 @@ public:
 
 // Function to verify the condition of having 2 stars in both a a row and a column
     bool verifyTwoStarsInRowAndColumn() {
-        for (int x = 0; x < 10; x++) {
+        for (int x = 0; x < 10; x++) {  // used to loop through rows of a 2D grid.
             int starsInRow = 0;
-            int starsInColumn = 0;
-            for (int y = 0; y < 10; y++) {
-                if (grid[x][y].hasStar) {
-                    starsInRow++;
+            int starsInColumn = 0;   // will be used to count the number of stars in the current column And row
+            for (int y = 0; y < 10; y++) {     //loop through the columns of the grid within the current row.
+                if (grid[x][y].hasStar) {      // checks if current row&Cololum has a star
+                    starsInRow++;              // if true, increase number of star by 1.
                 }
-                if (grid[y][x].hasStar) {
+                if (grid[y][x].hasStar) { 
                     starsInColumn++;
                 }
             }
-            if (starsInRow == 2 && starsInColumn == 2) {
-                return true;
+            if (starsInRow == 2 && starsInColumn == 2) { // check there are exactly 2 stars in both the current row and column
+                return true;                            // returns true if condition met or false otherwise
             }
         }
         return false;
